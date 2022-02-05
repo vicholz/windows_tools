7e19472314b86a3e7cc0e724212d928c081c815e
Victor <vicholz@gmail.com>
2022-02-05 14:19:40 -0800

    Update install_all.bat

--- post_install/install_all.bat
+++ post_install/install_all.bat
@@ -1,15 +1,18 @@
 @ECHO OFF
 
 echo "Importing Registry Configs..."
-reg import post_install/clock_show_seconds.reg
-reg import post_install/disable_lock_screen.reg
-reg import post_install/HKCUCSB_folder_settings.reg
-reg import post_install/HKCUSB_folder_settings.reg
-reg import post_install/lockscreen_disable_background.reg
+reg import clock_show_seconds.reg
+reg import disable_lock_screen.reg
+reg import HKCUCSB_folder_settings.reg
+reg import HKCUSB_folder_settings.reg
+reg import lockscreen_disable_background.reg
 echo "Importing Registry Configs...DONE!"
 echo
 echo "Copying ls/ll command shortcuts..."
 copy /y ls.cmd c:\windows\
 copy /y ll.cmd c:\windows\
 echo "Copying ls/ll command shortcuts...DONE!"
+echo
+echo "DONE!"
+pause
 exit

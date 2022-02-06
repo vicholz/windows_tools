70c9bb5fcf5080f148ce3c37e2ec065a937df99c
Victor Holz <vholz@salesforce.com>
2022-02-06 06:31:40 +0000

    cleaning up

--- post_install/install_all.bat
+++ post_install/install_all.bat
@@ -3,8 +3,6 @@ echo.
 echo Importing Registry Configs...
 reg import clock_show_seconds.reg
 reg import disable_lock_screen.reg
-reg import HKCUCSB_folder_settings.reg
-reg import HKCUSB_folder_settings.reg
 reg import lockscreen_disable_background.reg
 echo Importing Registry Configs...DONE!
 echo.

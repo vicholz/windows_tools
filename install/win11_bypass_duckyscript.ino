e4af73b1180e1e8e9e646c1e4d6a0bbdfad74794
Victor <vicholz@gmail.com>
2022-02-25 09:37:36 -0800

    Updating.

--- install/win11_bypass_duckyscript.ino
+++ install/win11_bypass_duckyscript.ino
@@ -5,16 +5,6 @@ int LED_PIN = 1;
 int DELAY = 100;
 boolean HAS_RUN = false;
 
-void sendKey(key){
-  DigiKeyboard.sendKeyStroke(key);
-  
-}
-
-void sendString(string){
-  DigiKeyboard.println(string);
-  DigiKeyboard.delay(DELAY);
-}
-
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
@@ -27,15 +17,20 @@ void loop() {
   if (!HAS_RUN){
     digitalWrite(LED_PIN, HIGH);
     DigiKeyboard.sendKeyStroke(0);
-    DigiKeyboard.sendKeyStroke(MOD_SHIFT_LEFT, KEY_F10);
+    //DigiKeyboard.sendKeyStroke(MOD_SHIFT_LEFT, KEY_F10);
     DigiKeyboard.delay(1000);
-    sendString("reg.exe add HKLM\SYSTEM\Setup\LabConfig");
-    sendString("reg.exe add HKLM\SYSTEM\Setup\LabConfig /v BypassTPMCheck /t REG_DWORD /d 1");
-    sendString("reg.exe add HKLM\SYSTEM\Setup\LabConfig /v BypassRAMCheck /t REG_DWORD /d 1");
-    sendString("reg.exe add HKLM\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /t REG_DWORD /d 1");
-    sendString("exit");
+    DigiKeyboard.println("reg.exe add HKLM\SYSTEM\Setup\LabConfig");
+    DigiKeyboard.println("reg.exe add HKLM\SYSTEM\Setup\LabConfig /v BypassTPMCheck /t REG_DWORD /d 1");
+    DigiKeyboard.println("reg.exe add HKLM\SYSTEM\Setup\LabConfig /v BypassRAMCheck /t REG_DWORD /d 1");
+    DigiKeyboard.println("reg.exe add HKLM\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /t REG_DWORD /d 1");
+    DigiKeyboard.println("exit");
     digitalWrite(LED_PIN, LOW);
     HAS_RUN = true;
+  } else {
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+    delay(100);
   }
 }
 
